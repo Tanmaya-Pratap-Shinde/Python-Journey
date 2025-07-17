e79ab2a1295fe78e38b1a2a12b4d899812d095d4
Tanmaya-Pratap-Shinde <tanmayashinde20@gmail.com>
2025-07-17 21:44:40 +0530

    Revert "Progress of Day 7"

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import argparse
 from scanner import ping
-from logger import new_log_result, new_log_error
-parser=argparse.ArgumentParser(description="\U0001F4E1 ReconBot CLI Scanner")
-parser.add_argument("--domain", type=str, help="Target domain to scan")
-parser.add_argument("--log", type=str, default="new_recon_log.txt", help="Output log file")
-parser.add_argument("--mode", choices=["safe","aggressive"], default="safe")
-args=parser.parse_args()
+from logger import log_result, log_error
+from config import TARGETS
 
-try:
-    result=ping(args.domain)
-    print("\U0001F4E1", result)
-    new_log_result(result, args.log)
-except Exception as e:
-    print("\u26A0 Error: ", e)
-    new_log_error(e)
+for domain in TARGETS:
+    try:
+        result=ping(domain)
+        print("\U0001F4E1", result)
+        log_result(result)
+    except Exception as e:
+        print("\u26A0 Error: ", e)
+        log_error(e)
